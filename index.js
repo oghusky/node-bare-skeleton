6f1c49fd5491d21a27c33d538c8d8545ecfdc1b5
Philip Simmons <philiposimmons@gmail.com>
2020-03-24 14:03:41 -0400

    shortened README file

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 const fs = require("fs");
 
 // takes in cb and creates a folder with argument name
-exports.mkdirFolder=(foldername, cb)=>{
+exports.mkdirFolder = (foldername, cb) => {
   cb(foldername);
 }
 
 // function to create gitignore file
-exports.makeGitIgnore = ()=>{
+exports.makeGitIgnore = () => {
   // writes to file what it shoud ignore once file is created
-  fs.writeFile(".gitignore", "node_modules/\npackage-lock.json", (err)=>{
-    if(err) throw err;
+  fs.writeFile(".gitignore", "node_modules/\npackage-lock.json", (err) => {
+    if (err) throw err;
     // logs if sucessfull
     console.log(".gitignore created");
   });
 }
 
-exports.createServerFile = ()=>{
+exports.createServerFile = () => {
   // writes server js file to root folder
-  fs.writeFile("index.js","require('dotenv').config();\nconst express = require('express');\nconst app = express();\nconst path = require('path');\napp.use(express.urlencoded({extended: true}));\napp.use(express.json());\napp.use(express.static(path.join(__dirname, 'public')));\nconst PORT = process.env.PORT || 5000;\napp.listen(PORT, ()=>{ console.log('Server listening on '+ PORT)})", (err)=>{
-    if(err) throw err;
+  fs.writeFile("index.js", "require('dotenv').config();\nconst express = require('express');\nconst app = express();\nconst path = require('path');\napp.use(express.urlencoded({extended: true}));\napp.use(express.json());\napp.use(express.static(path.join(__dirname, 'public')));\nconst PORT = process.env.PORT || 5000;\napp.listen(PORT, ()=>{ console.log('Server listening on '+ PORT)})", (err) => {
+    if (err) throw err;
     // logs if successful
     console.log("index.js created");
   })
 }
 
 // makes readme file
-exports.makeReadMe = ()=>{
+exports.makeReadMe = () => {
   // because second parameter is blank file is created and left blank
-  fs.writeFile("README.md", "## Created with Node-Bare-Skeleton\n", (err)=>{
-    if(err) throw err;
+  fs.writeFile("README.md", "## Created with Node-Bare-Skeleton\n", (err) => {
+    if (err) throw err;
     // logs if successful
     console.log("README.md created");
   })
 }
 
 // function to create folder
-exports.scaffoldFolder=(foldername)=>{
+exports.scaffoldFolder = (foldername) => {
   // because recursive is false if folders exist this will throw err
-  return fs.mkdir(`${foldername}`, {recursive:false}, (err)=>{
-    if(err) throw err;
+  return fs.mkdir(`${foldername}`, { recursive: false }, (err) => {
+    if (err) throw err;
     // logs if successful
     console.log(`${foldername} has been created`);
     // if folder is created successfully this creates the blank index file inside of it
-    fs.writeFile(`${foldername}/index.js`, "", (err)=>{
-      if(err) throw err;
+    fs.writeFile(`${foldername}/index.js`, "", (err) => {
+      if (err) throw err;
       // logs if successful
       console.log(`${foldername}/index.js created`);
     });
